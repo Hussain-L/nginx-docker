59c97b3a7c17d7e3c2e44dd87e93923799bddc20
Hussain J Lokhandwala <65801800+Hussain-L@users.noreply.github.com>
2022-09-28 14:13:24 +0530

    Update script.sh

--- Scripts/script.sh
+++ Scripts/script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /home/ubuntu
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 624852564786.dkr.ecr.ap-south-1.amazonaws.com
 docker pull 624852564786.dkr.ecr.ap-south-1.amazonaws.com/docker-delete:latest
